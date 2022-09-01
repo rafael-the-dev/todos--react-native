14927490f0d346273f48c15229d71960d497a7f3
Rafael Tivane <luislangabusiness@gmail.com>
2022-09-01 11:09:54 +0200

    kick off and create header

--- App.js
+++ App.js
@@ -1,20 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, ImageBackground, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+import mobileLightBg from "./assets/images/bg-mobile-light.jpg";
+import iconMoon from "./assets/images/icon-moon.svg";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <ImageBackground 
+                source={{ uri: mobileLightBg }}
+                style={styles.bgImage}
+            >
+                <View style={styles.headerContent}>
+                    <Text style={styles.title}>Todo</Text>
+                    <TouchableOpacity>
+                        <Image 
+                            source={{ uri: iconMoon }}
+                            style={styles.themeIcon}
+                        />
+                    </TouchableOpacity>
+                </View>
+            </ImageBackground>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+    bgImage: {
+        height: 200,
+        paddingHorizontal: '5%',
+        paddingTop: "1.2rem"
+    },
+    headerContent: {
+        alignItems: "center",
+        flexDirection: "row",
+        justifyContent: "space-between"
+    },
+    title: {
+        color: "#FFF",
+        fontSize: "1.4rem",
+        fontWeight: "bold",
+        letterSpacing: 4,
+        textTransform: "uppercase"
+    },
+    themeIcon: {
+        height: 15,
+        width: 15
+    }
 });
